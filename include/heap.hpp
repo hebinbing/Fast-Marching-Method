797ee031d76d3d80e215c7d617396f67c32ef674
Alexandre Rocha <avrocha99@gmail.com>
2020-11-12 18:37:13 +0000

    updated heap class : insert(), up_heap(), down_heap(), pop()

--- include/heap.hpp
+++ include/heap.hpp
@@ -1,39 +1,30 @@
-#include <iostream>
-#include <vector>
 #include <cmath>
 #include <cstdint>
+#include <iostream>
+#include <vector>
 
 namespace fmm {
 
 template <typename T> class min_heap {
   private:
-    struct node{
-      T value;
-      uint64_t index;
+    struct node {
+        T value;
+        uint64_t index;
     };
-    
+
     std::vector<node> data;
-    
+
     //! Updates the value of an existing node
     void update();
 
-    uint64_t parent_index(uint64_t index) { return floor((index - 1) / 2); }
+    uint64_t parent(uint64_t index) { return floor((index - 1) / 2); }
 
-    uint64_t left_child_index(uint64_t index) { return floor((index + 1) / 2); }
-
-    uint64_t right_child_index(uint64_t index) { return floor((index + 2) / 2); }
-
-    uint64_t min_child_index(uint64_t index) { return std::min(right_child_index(index), left_child_index(index)); }
-    
-    uint64_t parent(uint64_t index) { return data.at(parent_index(index)); }
-
-    uint64_t left_child(uint64_t index) { return data.at(left_child_index(index)); }
-
-    uint64_t right_child(uint64_t index) { return data.at(right_child_index(index)); }
-
-    uint64_t min_child(uint64_t index) { return data.at(min_child_index(index)); }
+    uint64_t min_child(uint64_t index);
 
   public:
+    //! Min_heap constructor
+    min_heap(uint64_t max_size);
+
     //! Returns the min node value
     T const min_node() {
         if (data.size() == 0)
@@ -53,13 +44,34 @@ template <typename T> class min_heap {
 
     //! Bubbles up a node
     void up_heap(uint64_t index);
+
+    //! Returns data vector size
+    uint64_t size() { return data.size(); }
+
+    //! Debug temp pruint64_t function
+    void print();
 };
 }
 
-template <typename T> 
-void fmm::min_heap<T>::update() {}
+template <typename T> fmm::min_heap<T>::min_heap(uint64_t max_size) {
+    data.reserve(max_size);
+}
 
-template <typename T> 
+template <typename T> uint64_t fmm::min_heap<T>::min_child(uint64_t index) {
+    if (data.size() <= 2 * index + 1) {
+        return index;
+
+    } else if (data.size() - 1 == 2 * index + 1) {
+        return 2 * index + 1;
+
+    } else {
+    return (data.at(2*index + 1).value <= data.at(2*index +2).value) ? (2*index + 1) : (2*index + 2);
+    }
+}
+
+template <typename T> void fmm::min_heap<T>::update() {}
+
+template <typename T>
 void fmm::min_heap<T>::insert(T new_node_value, uint64_t new_node_index) {
     node new_node;
 
@@ -71,40 +83,42 @@ void fmm::min_heap<T>::insert(T new_node_value, uint64_t new_node_index) {
     up_heap(data.size() - 1);
 }
 
-template <typename T> 
-void fmm::min_heap<T>::pop() {
-  if (data.size() == 0)
+template <typename T> void fmm::min_heap<T>::pop() {
+    if (data.size() == 0) {
         std::cerr << "Heap is empty." << std::endl;
+        return;
+    }
+
+    std::swap(data.at(0), data.at(data.size() - 1));
+    data.pop_back();
+
+    down_heap(0);
 }
 
-template <typename T> 
-void fmm::min_heap<T>::down_heap(uint64_t index) {
+template <typename T> void fmm::min_heap<T>::down_heap(uint64_t index) {
 
-  //for cycle that swaps parent w/ children when node.value is smaller
-      //compare node.value
+    for (uint64_t child = min_child(index);
+         index < data.size() && data.at(child).value < data.at(index).value;
+         index = child, child = min_child(index)) {
 
-  for(auto child = min_child(index); data[index].value != data.end().value; index = min_child_index(index)){
-    
-    if(data.at(index).value > child.value)
-      std::swap(data.at(index), data.at(child.index));
-    
-  }
-
-  //what to return?
+        std::swap(data.at(child), data.at(index));
+    }
 }
 
-template <typename T> 
-void fmm::min_heap<T>::up_heap(uint64_t index) {
+template <typename T> void fmm::min_heap<T>::up_heap(uint64_t index) {
 
-  //for cycle that swaps children w/ parent when node.value is smaller
-      //compare node.value
+    for (uint64_t par = parent(index);
+         index > 0 && data.at(index).value < data.at(par).value;
+         index = par, par = parent(index)) {
 
-  for(auto par = parent(index); data[index].value != data.begin().value; index = par.index){
-    
-    if(data.at(index).value < par.value)
-      std::swap(data.at(index), data.at(par.index));
-    
-  }
+        std::swap(data.at(index), data.at(par));
+    }
+}
 
-  //what to return?
+template <typename T> void fmm::min_heap<T>::print() {
+
+    for (auto n = data.begin(); n != data.end(); ++n) {
+        std::cout << "(" << n->index << "," << n->value << ") " << std::endl;
+    }
+    std::cout << "" << std::endl;
 }
--- test/test_heap.cpp
+++ test/test_heap.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <cstdlib>
 
 #include "../include/heap.hpp"
+
+using namespace fmm;
+
+//! Temp debug function
+void debug(char const *msg){
+    std::cout << msg << std::endl;
+}
 
 int main(){
     
     std::cout << "TEST FUNCTION!" << std::endl;
+    
+    min_heap<int>test(10);
+    int temp;
 
+    for(int i = 0; i < 10; i++){
+        temp = rand() % 100;
+
+        test.insert(temp,i);
+    }   
+    
+    debug("heap example");
+    test.print();
+
+    test.pop();
+    
+    debug("same heap after one pop()");
+    test.print();
 } 
