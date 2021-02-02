2c75deb90101eed32eca1b60df7be150c0f95892
Alexandre Rocha <avrocha99@gmail.com>
2021-02-02 16:59:12 +0000

    Last updates

--- examples/main_example.cpp
+++ examples/main_example.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 #include "hdf5_parser.hpp"
+#include <chrono>
+#include <fstream>
 
 int main()
 {
@@ -14,8 +16,15 @@ int main()
         io::read(file_name_r, dataset_name_r);
 
     fmm::solver_t s(cost_function);
-
+    auto start = std::chrono::high_resolution_clock::now();
     fmm::function<fmm::data_t> value_function = s.solve();
+    auto stop = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+    std::ofstream ofs("../data/last_measured_time.txt", std::ofstream::trunc);
+    ofs << duration.count();
+    ofs.close();
 
     io::write(file_name_w, dataset_name_w, value_function);
 
